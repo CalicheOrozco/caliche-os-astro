224eb4dc4ee4a0030ef1a9b590d62f7304b7dffc
Caliche Orozco <caliche2da1@gmail.com>
2023-09-13 18:11:08 -0600

    caliche-os with astro

---
+++ constants/data.js
@@ -0,0 +1,441 @@
+const myData = {
+  githubUsername: "CalicheOrozco",
+  name: "Caliche Orozco",
+  designation: "Front-End Developer",
+  avatarUrl: "/avatar.jpg",
+  email: "info@calicheorozco.com",
+  navmenu: [
+    {
+      name: "Projects",
+      url: "/projects",
+      icon: "../src/images/navbar/projects.ico",
+    },
+    {
+      name: "Skills",
+      url: "/skills",
+      icon: "../src/images/navbar/skills.ico",
+    },
+    {
+      name: "Experience",
+      url: "/experience",
+      icon: "../src/images/navbar/experencies.ico",
+    },
+    {
+      name: "About",
+      url: "/about",
+      icon: "../src/images/navbar/about (2).ico",
+    },
+    {
+      name: "Contact",
+      url: "/contact",
+      icon: "../src/images/navbar/contact.png",
+    },
+    {
+      name: "Resume",
+      url: "/resume",
+      icon: "../src/images/navbar/resume.ico",
+    },
+  ],
+  projects: [
+    {
+      title: "SpaceX Launches 🚀",
+      link: "https://spacex-launches-six.vercel.app/",
+      github: "https://github.com/CalicheOrozco/spacex-launches",
+      imgUrl: "../src/images/portafolio19.png",
+      stack: ["Astro", "Tailwind", "HTML/CSS", "Typescript"],
+    },
+    {
+      title: "Calichecraft",
+      link: "https://calichecraft.vercel.app/",
+      github: "https://github.com/CalicheOrozco/calichecraft",
+      imgUrl: "../src/images/portafolio18.png",
+      stack: ["React", "ThreeJS", "HTML/CSS", "Javascript"],
+    },
+    {
+      title: "Caliche Translate",
+      link: "https://caliches-translate.vercel.app/",
+      github: "https://github.com/CalicheOrozco/Caliches-Translate",
+      imgUrl: "../src/images/portafolio17.png",
+      stack: [
+        "Next.js",
+        "React",
+        "Tailwind",
+        "Typescript",
+        "Javascript",
+        "API",
+        "Redux",
+      ],
+    },
+    {
+      title: "Caliche Portfolio 2022",
+      link: "https://caliche-next.vercel.app/",
+      github: "https://github.com/CalicheOrozco/caliche-next",
+      imgUrl: "../src/images/portafolio16.png",
+      stack: ["Next.js", "React", "Tailwind", "Javascript"],
+    },
+    {
+      title: "Robocoons website",
+      link: "https://robocoons.vercel.app/",
+      github: "https://github.com/CalicheOrozco/robocoons",
+      imgUrl: "../src/images/portafolio14.png",
+      stack: ["React", "Tailwind", "Javascript", "HTML/CSS"],
+    },
+    {
+      title: "English Certify by Caliche",
+      link: "https://www.englishcertify.com/",
+      github: "https://github.com/CalicheOrozco/duolingo-test-practice-caliche",
+      imgUrl: "../src/images/portafolio13.png",
+      stack: ["React", "Tailwind", "Javascript", "HTML/CSS"],
+    },
+    {
+      title: "Memory Game by Caliche",
+      link: "https://memory-game-caliche.vercel.app/",
+      github: "https://github.com/CalicheOrozco/memory-game",
+      imgUrl: "../src/images/portafolio12.png",
+      stack: ["React", "Tailwind", "Javascript", "HTML/CSS"],
+    },
+    {
+      title: "Caliche Productivity APP",
+      link: "https://productivity-calicheapp.vercel.app/",
+      github: "https://github.com/CalicheOrozco/productivity-calicheapp",
+      imgUrl: "../src/images/portafolio15.png",
+      stack: ["React", "Tailwind", "Javascript", "HTML/CSS"],
+    },
+    {
+      title: "English Tenses by Caliche",
+      link: "https://english-tenses-caliche.vercel.app/",
+      github: "https://github.com/CalicheOrozco/english-tenses-caliche",
+      imgUrl: "../src/images/portafolio11.jpg",
+      stack: ["React", "Tailwind", "Javascript", "HTML/CSS", "API"],
+    },
+    {
+      title: "Caliche weather APP",
+      link: "https://caliche-weather-app.vercel.app/",
+      github: "https://github.com/CalicheOrozco/english-tenses-caliche",
+      imgUrl: "../src/images/portafolio10.jpg",
+      stack: ["React", "Tailwind", "Javascript", "HTML/CSS", "API"],
+    },
+    {
+      title: "Landingpage 2021",
+      link: "https://calicheorozco-old-page.vercel.app/",
+      github: "https://github.com/CalicheOrozco/calicheorozco-old-page",
+      imgUrl: "../src/images/portafolio8.jpg",
+      stack: ["React", "Tailwind", "Javascript", "HTML/CSS"],
+    },
+
+    {
+      title: "Caliche MOVIE API",
+      link: "https://caliche-movies-calicheorozco.vercel.app/",
+      github: "https://github.com/CalicheOrozco/caliche-movies",
+      imgUrl: "../src/images/portafolio9.jpg",
+      stack: ["HTML/CSS", "Javascript", "API"],
+    },
+
+    {
+      title: "Futurnow Landingpage",
+      link: "https://futurnow-caliche-calicheorozco.vercel.app/",
+      github: "https://github.com/CalicheOrozco/FutrNow",
+      imgUrl: "../src/images/portafolio6.jpg",
+      stack: ["React", "Javascript", "Bootstrap", "HTML/CSS"],
+    },
+    {
+      title: "Bookmark App",
+      link: "https://bookmark-app-caliche-calicheorozco.vercel.app/",
+      github: "https://github.com/CalicheOrozco/evaluacion-bedu",
+      imgUrl: "../src/images/portafolio7.jpg",
+      stack: ["React", "Javascript", "Bootstrap", "HTML/CSS"],
+    },
+    {
+      title: "SALSA CATRINA",
+      link: "https://salsa-catrina.surge.sh",
+      github:
+        "https://github.com/CalicheOrozco/Desarrollo-web/tree/master/LandingPages/Salsas-la-catrina",
+      imgUrl: "../src/images/portafolio1.jpg",
+      stack: ["HTML/CSS", "Bootstrap"],
+    },
+    {
+      title: "PLATZICONF MEXICO",
+      link: "https://platziconfmexico.surge.sh/",
+      github:
+        "https://github.com/CalicheOrozco/Desarrollo-web/tree/master/LandingPages/Platzi-bootstrap",
+      imgUrl: "../src/images/portafolio2.jpg",
+      stack: ["HTML/CSS", "Bootstrap"],
+    },
+    {
+      title: "Arduino Day 2021",
+      link: "https://countdownarduino21-caliche.surge.sh",
+      github:
+        "https://github.com/CalicheOrozco/Desarrollo-web/tree/master/Countdown/2021/countdown",
+      imgUrl: "../src/images/portafolio5.jpg",
+      stack: ["HTML/CSS", "Bootstrap", "Javascript"],
+    },
+    {
+      title: "WWE NETWORK",
+      link: "https://wwenetworkcaliche.surge.sh/",
+      github:
+        "https://github.com/CalicheOrozco/Desarrollo-web/tree/master/WWE%20Network",
+      imgUrl: "../src/images/portafolio3.jpg",
+      stack: ["HTML/CSS", "Javascript", "Bootstrap"],
+    },
+    {
+      title: "SIMON DICE",
+      link: "https://simondice-caliche.surge.sh/",
+      github:
+        "https://github.com/CalicheOrozco/Desarrollo-web/tree/master/SimonDice",
+      imgUrl: "../src/images/portafolio4.jpg",
+      stack: ["HTML/CSS", "Javascript"],
+    },
+  ],
+  skills: {
+    description:
+      "This is my current tech stack, in which I feel most comfortable and where I'm trying to specialize right now.",
+    languages: [
+      {
+        name: "HTML",
+        icon: "../src/images/icons/html.ico",
+      },
+      {
+        name: "CSS",
+        icon: "../src/images/icons/css.ico",
+      },
+      {
+        name: "Javascript",
+        icon: "../src/images/icons/javascript.ico",
+      },
+    ],
+    libraries: [
+      {
+        name: "Node.js",
+        icon: "../src/images/icons/nodejs.ico",
+      },
+      {
+        name: "Tailwind CSS",
+        icon: "../src/images/icons/tailwind.ico",
+      },
+      {
+        name: "React Query",
+        icon: "../src/images/icons/react-query.ico",
+      },
+      {
+        name: "GraphQL",
+        icon: "../src/images/icons/graphql.ico",
+      },
+      {
+        name: "Typescript",
+        icon: "../src/images/icons/typescript.ico",
+      },
+      {
+        name: "Bootstrap",
+        icon: "../src/images/icons/bootstrap.ico",
+      },
+    ],
+    frameworks: [
+      {
+        name: "React.js",
+        icon: "../src/images/icons/react.ico",
+      },
+      {
+        name: "Next.js",
+        icon: "../src/images/icons/nextjs.ico",
+      },
+      {
+        name: "Flask",
+        icon: "../src/images/icons/flask.ico",
+      },
+    ],
+  },
+  about: {
+    title:
+      "I am a middle-level front-end developer with 2 years of experience in creating responsive web designs and mobile apps that are user-friendly for startups. Currently, I am working on ",
+    titleEs:
+      "Soy un middle-level front-end developer con 2 años de experiencia en la creación de diseños web receptivos y aplicaciones móviles fáciles de usar para startups. Actualmente, estoy trabajando en ",
+    description: [
+      ` I am a frontend developer with experience developing websites and applications. I have strong knowledge in technologies like React, TailwindCSS, Next JS, TypeScript and GraphQL. My focus in development is to create quality, efficient and scalable solutions to meet the needs of users. I am a frontend developer with experience developing websites and applications. I have strong knowledge in technologies like React, TailwindCSS, Next JS, TypeScript and GraphQL. My focus in development is to create quality, efficient and scalable solutions to meet the needs of users.`,
+      `In my career, I have worked in interdisciplinary teams in different industries, including education, consulting, and foreign trade. Contributed to the creation of various web products in collaboration with product and back-end teams, and helped improve website performance by 50%.`,
+      `I also have a solid understanding of UX/UI best practices and have implemented agile methodologies, such as Scrum, to optimize speed and scalability of applications. My commitment to work and detail has been shown in the development of reusable code and libraries for future projects.`,
+      `In addition, I am fluent in English, which has allowed me to work with international teams and broaden my professional horizons. I am committed to continue learning and developing my skills to create impactful and efficient solutions for my clients.`,
+      `If you are looking for an experienced fron-tend developer with a strong commitment to quality, don't hesitate to contact me.`,
+    ],
+    descriptionEs: [
+      `He estado desarrollando páginas web desde que tenía 12 años. No sabía qué significaba ser desarrollador front-end en ese momento porque el término no se acuñaba en ese tiempo. `,
+      `A medida que adquiría más experiencia, amplié mi conjunto de habilidades para incluir tecnologías modernas como React, Next JS, TypeScript, React-hook-form, Editor.js, Node.js, MySQL, Bootstrap y Tailwind, entre otras.`,
+    ],
+    currentCompany: "NTT Data",
+    currentProject: "Tenaris NMS Order Dressing",
+    currentProjectUrl: "https://github.com/bedu-hq",
+    education: "Veracruz Technological Institute",
+    languages: "Spanish and English",
+  },
+  experience: [
+    {
+      title: "Centers Senior Developer",
+      icon: "../src/images/companies/ntt.jpeg",
+      company: "NTT Data",
+      location: "León, Guanajuato, Mexico",
+      startDate: "Mar 2023",
+      endDate: "Currently",
+      companyLink: "https://mexico.nttdata.com/",
+      desc: [
+        "Project: Tenaris Tamsa NMS Project",
+        " • Managed customer specifications and international steel pipe standards to maintain manufacturing systems.",
+        " • Developed desktop applications to streamline and expedite the production order creation process.",
+        " • Analyzed and revised specifications, standards, and orders for production order creation.",
+        " • Coordinated production order receipt from global planning to fulfill customer expectations.",
+        " • Oversaw logistics and processing of customer orders and product designs.",
+      ],
+      skills: "C# · .NET · SQL ",
+    },
+    {
+      title: "Front-End Developer",
+      icon: "../src/images/companies/bedu.png",
+      company: "BEDU",
+      location: "Mexico City, Mexico",
+      startDate: "Dec 2021",
+      endDate: "Jun 2021",
+      companyLink: "https://bedu.org/",
+      desc: [
+        "• Developed websites using React and TailwindCSS, collaborating with UX/UI design teams to enhance website performance by 50%.",
+        "• Contributed to creating various web products in partnership with product and backend teams.",
+        "• Participated in product releases and code reviews, working closely with 3 senior developers and team leads",
+        " • Demonstrated strong work ethic and attention to detail.",
+        " • Assisted in developing 100% reusable code and libraries for future projects.",
+        "• Demonstrated strong work ethic and attention to detail.",
+      ],
+      skills:
+        "Tailwind CSS · TypeScript · Node.js · ReactQuery · GraphQL · Next.js · JavaScript · HTML · React.js",
+    },
+    {
+      title: "Apprenticeship",
+      icon: "../src/images/companies/scio.png",
+      company: "Scio Consulting",
+      location: "Morelia, Michoacán, México",
+      startDate: "Sep 2021",
+      endDate: "Dec 2021",
+      companyLink: "https://sciodev.com/",
+      desc: [
+        "•	Strong understanding of UX/UI best practices.",
+        "•	Acquired and implemented Scrum methodology.",
+        "•	Collaborated with product teams on new product launches.",
+        "•	Optimized applications for optimal speed and scalability.",
+        "•	Addressed and reduced technical debt by 30%.",
+      ],
+      skills:
+        "Scrum · Cascading Style Sheets (CSS) · JavaScript · HTML · React.js",
+    },
+    {
+      title: "Front-end Internship",
+      icon: "../src/images/companies/logiety.png",
+      company: "Logiety",
+      location: " Veracruz, México",
+      startDate: "Feb 2021",
+      endDate: "Dec 2021",
+      companyLink: "https://logiety.com/",
+      desc: [
+        "•	Developed landing pages using React.",
+        "•	Created templates in Openpyxl, which significantly reduced the time required for form filling by 90%.",
+        "•	Implemented CSS effects to enhance user interaction with the website.",
+        "•	Contributed to the development of new features.",
+      ],
+      skills:
+        "Skills: Cascading Style Sheets (CSS) · JavaScript · HTML · React.js · Openpyxl · Python",
+    },
+    {
+      title: "Help Newcomers and promoter of Latino America culture",
+      icon: "../src/images/companies/nb.jpeg",
+      company: "Hola New Brunswick",
+      location: "Moncton, New Brunswick, Canada",
+      startDate: "Jun 2022",
+      endDate: "Nov 2022",
+      companyLink: "https://www.facebook.com/HolaNewBrunswick",
+      desc: [
+        "•	Volunteered with the nonprofit organization Hola New Brunswick to assist newcomers from Latin America in Moncton in integrating into the community of New Brunswick.",
+        "• Collaborated in organizing and promoting cultural events such as the Mosaiq Moncton Festival 2022, showcasing Latin American culture.",
+      ],
+    },
+    {
+      title: "IEEE ITVER",
+      icon: "../src/images/companies/ieee.png",
+      location: " Veracruz, México",
+      company: "Institute of Electrical and Electronics Engineers ITVER.",
+      startDate: "Sep 2017",
+      endDate: "Feb 2021",
+      companyLink: "https://www.facebook.com/ieee.itver/",
+      desc: [
+        "•	Dedicated IEEE volunteer and Vice President at my university for 2 years.",
+        "• Led various initiatives aimed at promoting technology and science.",
+        "• Organized events such as the Veracruz Children's Science Fair 2017.",
+        "• Participated in the IEEE Day celebrations from 2018 to 2020 in Veracruz.",
+        '• Conducted a Scratch Day workshop at "Las Hayas" school.',
+      ],
+    },
+    {
+      title: "Pro sustainability",
+      icon: "../src/images/companies/estudiantes.jpg",
+      company: "ITVER Green Students.",
+      location: " Veracruz, México",
+      startDate: "Aug 2017",
+      endDate: "Jun 2022",
+      companyLink: "https://www.facebook.com/EstudiantesVerdesMx",
+      desc: [
+        "Founded and led various initiatives focused on sustainable development.",
+        " • Organized beach cleanups to promote environmental conservation.",
+        " • Provided support for marginalized communities through initiatives and projects.",
+        " • Led reforestation efforts in parks to enhance green spaces and biodiversity.",
+        " • Conducted hospital visits to bring joy and support to patients.",
+        " • Organized the collection and recycling of materials to promote waste reduction and sustainability.",
+      ],
+    },
+    {
+      title: "IT culture coordinator and promoter",
+      icon: "../src/images/companies/veracruz.png",
+      company: "Veracruz",
+      location: " Veracruz, México",
+      startDate: "Sep 2017",
+      endDate: "Dec 2021",
+      companyLink: "https://es.wikipedia.org/wiki/Veracruz",
+      desc: [
+        "• Coordinated and organized Talent Nights Veracruz events from 2017 to 2019.",
+        "• Organized Arduino Day Veracruz events in 2018 and 2019.",
+        "• Coordinated Hacker Day event in 2019.",
+        "• Organized the Tecnovation Challenge Veracruz.",
+        "• Arranged Technological Meetups Veracruz.",
+      ],
+    },
+    {
+      title: "Conferences and Workshops",
+      icon: "../src/images/companies/veracruz.png",
+      company: "Veracruz",
+      location: " Veracruz, México",
+      startDate: "Sep 2019",
+      endDate: "Dec 2021",
+      companyLink: "https://es.wikipedia.org/wiki/Veracruz",
+      desc: [
+        "Workshop facilitator and conference speaker in the following topics:",
+        "• IOT with Raspberry Pi and Python.",
+        "• Augmented reality with Spark AR Studio.",
+        "• Introduction to Raspberry Pi.",
+        "• How can I be an agent of change in my locality?",
+        "• IEEE As a student branch.",
+      ],
+    },
+  ],
+  socialLinks: [
+    {
+      name: "LinkedIn",
+      url: "https://www.linkedin.com/in/calicheorozco/",
+      icon: "../src/images/social/linkedin.ico",
+    },
+    {
+      name: "GitHub",
+      url: "https://github.com/CalicheOrozco",
+      icon: "../src/images/social/github.ico",
+    },
+    {
+      name: "Mail",
+      url: "mailto:info@calicheorozco.com",
+      icon: "../src/images/social/mail.png",
+    },
+  ],
+};
+
+export default myData;
